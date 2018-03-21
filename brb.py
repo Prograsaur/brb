0397f42e456da897d2d517388e5df4f4a33f1cee
Prograsaur <prograsaur@gmail.com>
2018-03-20 23:30:04 -0400

    Progect started - Basic python code added to the main brb file.

---
+++ brb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Author: Sergey Ishin (Prograsaur) (c) 2018
+#-----------------------------------------------------------------------------
+
+'''
+Interactive Brokers TWS API -- "The Big Red Button" - one button to cancel all orders and close all positions.
+'''
+
+#region import
+import sys
+#endregion import
+
+#region main
+#-------------------------------------------------------------------------------
+def main():
+    return -1
+
+if __name__ == "__main__":
+    sys.exit(main())
+#endregion main
